0901197ba88c82c08f23998d931adcfad74025e0
montanha <diogokmgiordano@gmail.com>
2024-10-30 14:52:52 -0300

    adicionado o peso nas arestas com base no tempo de amizade

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import json
 import subprocess
 import sys
+from datetime import datetime
+
 
 # funcao para realizar a instalacao de um pacote (networkx)
 def install_package(package):
@@ -28,15 +30,63 @@ except ModuleNotFoundError:
     install_package("matplotlib")
 
 
-# Cria a conexao entre cada usuario baseado nas amizades do dataset (Sem peso de tempo ainda)
+#cria a conexao entre cada usuario baseado nas amizades do dataset e retorna uma lista com peso de cada aresta (Pode ser extraido outros metodos apartir desse)
 def connect_friends_in_graph():
+    # lista que vai conter os nodos e o peso entre eles
+    lista = []
+
+    dataset_amizades = dataset["amizades"]
+
+
     for node in grafo.nodes():
-        for i in range(len(dataset["amizades"])):
-            if grafo.nodes[node]["id"] == dataset["amizades"][i]["usuario1_id"]:
-                grafo.add_edge(node, dataset["amizades"][i]["usuario2_id"])
-                print("Nome do usuario: ", grafo.nodes[node]["nome"])
-                print("Id usuario 1: ", dataset["amizades"][i]["usuario1_id"])
-                print("Id Usuario 2: ", dataset["amizades"][i]["usuario2_id"])
+        for i in range(len(dataset_amizades)):
+            if grafo.nodes[node]["id"] == dataset_amizades[i]["usuario1_id"]:
+                # calcula o tempo de amizade em dias e com base no numero de dias calcula um peso
+                tempo = datetime.now() - datetime.strptime(dataset_amizades[i]["data_inicio"], "%Y-%m-%d")
+                peso = calculate_friendship_time(tempo.days)
+
+                # adiciona uma aresta que conecta os dois nodos
+                grafo.add_edge(node, dataset_amizades[i]["usuario2_id"])
+
+                # prints de teste
+                # printa o nome do usuario da iteracao e o id dos dois usuarios que possuem uma amizade
+                print("\nNome do usuario: ", grafo.nodes[node]["nome"])
+                print("Id usuario 1: ", dataset_amizades[i]["usuario1_id"])
+                print("Id Usuario 2: ", dataset_amizades[i]["usuario2_id"])
+                print("Peso da amizade: ", peso)
+
+                # adiciona na lista os dados de user 1 e 2, e o peso da realacao deles
+                lista.append((dataset_amizades[i]["usuario1_id"], dataset_amizades[i]["usuario2_id"], peso))
+
+
+    return lista
+
+#faz a apresentacao do grafico
+def present(grafo, lista):
+
+    # converte a lista para um dicionario
+    edge_labels = {(source, target): count for source, target, count in lista}
+
+    # posicao das arestas
+    pos = nx.spring_layout(grafo)
+
+    # desenha o grafo
+    nx.draw(grafo, pos, with_labels=True)
+
+    # peso das arestas
+    nx.draw_networkx_edge_labels(grafo, pos, edge_labels=edge_labels)
+
+    plt.show()
+
+#calcula o peso com base no tempo (Em dias) de amizade
+def calculate_friendship_time(days):
+    contador = 0
+
+    while days > 200:
+        contador += 1
+        days -= 200
+
+    return contador
 
 
 if __name__ == '__main__':
@@ -58,8 +108,7 @@ if __name__ == '__main__':
     for i in grafo.nodes():
         print("Nome do usuario: ", grafo.nodes[i]["nome"])
 
-    connect_friends_in_graph()
-
-    nx.draw(grafo, with_labels=True, node_color='skyblue', node_size=700, edge_color='black', linewidths=1,
-            font_size=15)
-    plt.show()
+    # recebe a lista retornada
+    lista = connect_friends_in_graph()
+    
+    present(grafo, lista)
